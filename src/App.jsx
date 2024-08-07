08327906fe7345da5f405d239fb8e288e31cc4bb
Biresh <js.biresh@gmail.com>
2024-08-07 16:48:34 +0530

    added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>Hello Rafi, Contact Page</div>
+      <Navigation />
     </>
   );
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+// import logo from '../images/logo.png'
+
+const Navigation = () => {
+  return (
+    <nav>
+      <div>
+        <img src="../images/logo.png" alt="Logo" />
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
